aef29c8cddddb1785075d1f8f935a44112b611a3
sebastien chabrolles <s.chabrolles@fr.ibm.com>
2015-06-22 15:55:16 +0100

    Bug sudo missing before cp grafana.db

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ case $SYS_ARCH in
 	x86_64) curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 	;;
 	
-	ppc64le) sudo apt-get update && sudo apt-get upgrade && sudo apt-get -y install docker.io
+	ppc64le) sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y install ntp docker.io
 	;;
 	
 	*) echo "Arch $SYS_ARCH not supported" 
@@ -61,7 +61,7 @@ echo
 
 
 [ ! -d /var/lib/grafana/dashboards ] && sudo mkdir -p /var/lib/grafana/dashboards
-[ -f grafana.db ] && cp -rp grafana.db /var/lib/grafana
+[ -f grafana.db ] && sudo cp -rp grafana.db /var/lib/grafana
 [ ! -d /var/lib/graphite/storage/whisper ] && sudo mkdir -p /var/lib/graphite/storage/whisper
 
 [ ! -d /etc/LiMon ] && sudo mkdir -p /etc/LiMon
