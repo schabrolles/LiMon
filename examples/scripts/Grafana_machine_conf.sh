783bf2fb451c1da2b1395bc545ced6c949aa93e6
sebastien chabrolles <s.chabrolles@fr.ibm.com>
2016-04-03 11:08:16 +0200

    add Grafana_machine.conf

---
+++ examples/scripts/Grafana_machine_conf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# s.chabrolles@fr.ibm.com
+#########################
+
+SMT=`lscpu | awk '/^Thread/ { print $NF}'`
+#SMT=`ppc64_cpu --smt | awk -F= '{ print $NF }'`
+#CORES=`lscpu | awk '/^Core/ { CORESOCKET=$NF }; /^Socket/ { SOCKET=$NF } END { print CORESOCKET * SOCKET }'`
+CORES=`/usr/sbin/ppc64_cpu --cores-on | awk '{ print $NF }'`
+NUMA_NODE=`lscpu | awk ' /NUMA node\(s\)/ { print $NF}'`
+FREQ=`grep -U MHz /proc/cpuinfo | sort -u | awk '{ split($NF,FREQ,".") ; print FREQ[1]}'`
+
+echo "SMT=$SMT
+CORES=$CORES
+NUMA_NODE=$NUMA_NODE
+FREQ=$FREQ" > /var/log/machine.conf
