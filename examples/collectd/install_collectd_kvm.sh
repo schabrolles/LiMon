5d95f7ee17d01392c151af5832fb63879f4c8999
Julien Limodin <julien.limodin@fr.ibm.com>
2016-10-20 12:29:43 +0200

    Add zswap collectd configuration

--- examples/collectd/install_collectd_kvm.sh
+++ examples/collectd/install_collectd_kvm.sh
@@ -6,6 +6,7 @@
 yum --enablerepo epel install -y collectd collectd-sensors collectd-virt
 
 semodule -i mypol.pp
+semodule -i collectd_zswap.pp
 
 cp 01_collectd.conf 02_sensors.conf /etc/collectd.d
 
